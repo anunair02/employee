1f4acf6d08ff919e122eb5718235e3021acc9361
anunair02 <anunair1302@gmail.com>
2017-09-19 17:24:54 +0530

    exception

---
+++ eclipse-workspace/ExceptionHandling/src/exceptionHandlingStories/ExceptionHandlingDemo2.java
@@ -0,0 +1,19 @@
+package exceptionHandlingStories;
+
+public class ExceptionHandlingDemo2 {
+
+	public static void main(String[] args) {
+		
+			  try{
+			  int data=25/0;
+			   System.out.println(data);
+			  }
+			  catch(ArithmeticException e){System.out.println("exception occured");}
+			 
+			  finally{System.out.println("finally block is always executed");}
+			 
+			  System.out.println("rest of the code...");
+			  }
+			
+}
+
