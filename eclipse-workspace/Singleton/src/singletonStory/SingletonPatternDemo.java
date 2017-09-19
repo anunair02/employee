94afdd4b3a18d3e86f6c8533110c68e6a11b62f3
anunair02 <anunair1302@gmail.com>
2017-09-19 17:22:15 +0530

    singleton

---
+++ eclipse-workspace/Singleton/src/singletonStory/SingletonPatternDemo.java
@@ -0,0 +1,13 @@
+package singletonStory;
+
+public class SingletonPatternDemo {
+
+	public static void main(String[] args) {
+
+	      SingleObject object = SingleObject.getInstance();
+	      object.showMessage();
+	   }
+	
+	}
+
+
